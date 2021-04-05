597e1e2f4e40f9e443e985f01993ee15b0d780b0
yumayamaguchi <kikkube@yahoo.co.jp>
2021-04-05 16:10:36 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- headerここから -->
+    <header>
+        <p><a href="index.php">卓球ツール</a></p>
+        <ul>
+            <li>会員登録</li>
+            <li>ログイン</li>
+        </ul>
+    </header>
+    <!-- headerここまで -->
+    <!-- main_barここから -->
+    <div id="main_bar">
+        <ul>
+            <li><a href="">ラケット</a></li>
+            <li><a href="">ラバー</a></li>
+            <li><a href="">検索ツール</a></li>
+        </ul>
+    </div>
+    <!-- main_barここまで -->
+    <!-- side_barここから -->
+    <div id="side_bar">
+        <ul>
+            <li><a>攻撃用シェーク</a></li>
+            <li><a>守備用シェーク</a></li>
+            <li><a>合板シェーク</a></li>
+        </ul>
+    </div>
+    <!-- side_barここまで -->
+    <!-- main_visualここから -->
+    <div id="main_visual">
+        <div class="">
+            <h3>攻撃用シェーク</h3>
+            <a href="">
+                <div class="">
+                    <img href="">
+                    <p></p>
+                </div>
+            </a>
+        </div>
+        <div class="">
+            <h3>守備用シェーク</h3>
+            <a href="">
+                <div class="">
+                    <img href="">
+                    <p></p>
+                </div>
+            </a>
+        </div>
+        <div class="">
+            <h3>合板シェーク</h3>
+            <a href="">
+                <div class="">
+                    <img href="">
+                    <p></p>
+                </div>
+            </a>
+        </div>
+    </div>
+    <!-- main_visualここまで -->
+    <!-- フッターここから -->
+    <footer>
+        <p>PAGE TOP</p>
+    </footer>
+    <!-- フッターここまで -->
+</body>
+
+</html>
