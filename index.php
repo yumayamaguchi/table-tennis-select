4a5efcd59b61a0bfaa5b6e953fc3159cb79ff228
yumayamaguchi <kikkube@yahoo.co.jp>
2021-04-06 20:26:58 +0900

    second_commit

--- index.php
+++ index.php
@@ -4,40 +4,55 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 
 <body>
     <!-- headerここから -->
     <header>
-        <p><a href="index.php">卓球ツール</a></p>
-        <ul>
-            <li>会員登録</li>
-            <li>ログイン</li>
-        </ul>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="head col-md-6">
+                    <p><a href="index.php">卓球ツール</a></p>
+                </div>
+                <div class="head_1 col-md-6">
+                    <ul>
+                        <li>会員登録</li>
+                        <li>ログイン</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </header>
     <!-- headerここまで -->
     <!-- main_barここから -->
     <div id="main_bar">
         <ul>
-            <li><a href="">ラケット</a></li>
-            <li><a href="">ラバー</a></li>
-            <li><a href="">検索ツール</a></li>
+            <a href=""><li>ラケット</li></a>
+            <a href=""><li>ラバー</li></a>
+            <a href=""><li>検索ツール</li></a>
         </ul>
     </div>
     <!-- main_barここまで -->
     <!-- side_barここから -->
+    <div class="center">
     <div id="side_bar">
         <ul>
-            <li><a>攻撃用シェーク</a></li>
-            <li><a>守備用シェーク</a></li>
-            <li><a>合板シェーク</a></li>
+            <li><a href=""><i class="far fa-check-circle"></i>攻撃用シェーク</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>守備用シェーク</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>合板シェーク</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>日本式ペン</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>中国式ペン</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>反転式ペン</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>単板ペン</a></li>|
+            <li><a href=""><i class="far fa-check-circle"></i>合板ペン</a></li>|
         </ul>
     </div>
     <!-- side_barここまで -->
     <!-- main_visualここから -->
     <div id="main_visual">
         <div class="">
-            <h3>攻撃用シェーク</h3>
+            <p>攻撃用シェーク</p>
             <a href="">
                 <div class="">
                     <img href="">
@@ -46,7 +61,7 @@
             </a>
         </div>
         <div class="">
-            <h3>守備用シェーク</h3>
+            <p>守備用シェーク</p>
             <a href="">
                 <div class="">
                     <img href="">
@@ -55,7 +70,7 @@
             </a>
         </div>
         <div class="">
-            <h3>合板シェーク</h3>
+            <p>合板シェーク</p>
             <a href="">
                 <div class="">
                     <img href="">
@@ -63,6 +78,7 @@
                 </div>
             </a>
         </div>
+    </div>
     </div>
     <!-- main_visualここまで -->
     <!-- フッターここから -->
@@ -70,6 +86,8 @@
         <p>PAGE TOP</p>
     </footer>
     <!-- フッターここまで -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
 </body>
 
 </html>
