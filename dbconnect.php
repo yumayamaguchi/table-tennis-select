38d233783f28d3840939a0c431fb89c249a12dd5
yumayamaguchi <80366714+yumayamaguchi@users.noreply.github.com>
2021-06-13 14:20:54 +0900

    デプロイ

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=table_tennis_tool;host=localhost;charset=utf8','root','root');
+    $db = new PDO('mysql:dbname=heroku_4db747f7713360b;host=us-cdbr-east-04.cleardb.com;charset=utf8','ba3aa41d627faa','792de597');
 } catch(PDOException $e) {
     print('DB接続エラー：' . $e->getMessage());
 }
